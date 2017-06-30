9c5d15da14e30a096e1e73f8def138499b5aab2d
askoldbaran <askold.b@gmail.com>
2017-06-30 10:46:05 +0300

    Added all repositories

---
+++ src/main/java/com/softserve/academy/spaced/repetition/repository/CourseRepository.java
@@ -0,0 +1,12 @@
+package com.softserve.academy.spaced.repetition.repository;
+
+import com.softserve.academy.spaced.repetition.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by askol on 6/30/2017.
+ */
+@Repository
+public interface CourseRepository extends JpaRepository<Account, Long> {
+}
