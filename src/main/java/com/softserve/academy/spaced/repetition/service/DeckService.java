d0c51895629f32dc43b5baf7a4548c01516d68b6
Vlad <vkiro@yandex.ua>
2017-07-06 12:37:01 +0300

    Added HTTP status to post methods in Course and Deck controllers, refactored Course and Deck services.

--- src/main/java/com/softserve/academy/spaced/repetition/service/DeckService.java
+++ src/main/java/com/softserve/academy/spaced/repetition/service/DeckService.java
@@ -1,12 +1,12 @@
 package com.softserve.academy.spaced.repetition.service;
 
 import com.softserve.academy.spaced.repetition.domain.Card;
+import com.softserve.academy.spaced.repetition.domain.Category;
 import com.softserve.academy.spaced.repetition.domain.Deck;
 import com.softserve.academy.spaced.repetition.repository.DeckRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -14,33 +14,34 @@ public class DeckService {
     @Autowired
     private DeckRepository deckRepository;
 
-    public List<Deck> getAllDecksByCategoryId(Long id) {
-        return deckRepository.getAllDecksByCategoryId(id);
+    public List<Deck> getAllDecksByCategoryId(Long category_id) {
+        return deckRepository.getAllDecksByCategoryId(category_id);
     }
 
-    public Deck getDeck(Long id) {
-        return deckRepository.findOne(id);
-    }
-
-    @Transactional
-    public List<Card> getAllCardsByDeckId(Long id) {
-        Deck deck = deckRepository.findOne(id);
-        return deck.getCards();
-    }
-
-    public void addDeck(Deck deck) {
-        deckRepository.save(deck);
+    public Deck getDeck(Long deck_id) {
+        return deckRepository.findOne(deck_id);
     }
 
     public List <Deck> findTop4ByOrderById() {
         return deckRepository.findTop4ByOrderById();
     }
 
-    /*public void updateDeck(Long id, DeckPublic deck) {
+    public List<Card> getAllCardsByDeckId(Long deck_id) {
+        Deck deck = deckRepository.findOne(deck_id);
+        return deck.getCards();
+    }
+
+    public void addDeck(Deck deck, Long category_id) {
+        deck.setCategory(new Category(category_id));
         deckRepository.save(deck);
     }
 
-    public void deleteDeck(Long id) {
-        deckRepository.delete(id);
-    }*/
+    public void updateDeck(Deck deck, Long deck_id) {
+        deck.setId(deck_id);
+        deckRepository.save(deck);
+    }
+
+    public void deleteDeck(Long deck_id) {
+        deckRepository.delete(deck_id);
+    }
 }
