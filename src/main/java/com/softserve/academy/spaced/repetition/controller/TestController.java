dfa4baff4660d79828a73f6230df947425116937
zadorovskyi <zadorovskyi@gmail.com>
2017-07-19 22:09:30 +0300

    changed url in Mailservice

--- src/main/java/com/softserve/academy/spaced/repetition/controller/TestController.java
+++ src/main/java/com/softserve/academy/spaced/repetition/controller/TestController.java
@@ -1,16 +1,21 @@
 package com.softserve.academy.spaced.repetition.controller;
 
-import org.springframework.security.access.prepost.PreAuthorize;
+import com.softserve.academy.spaced.repetition.domain.Deck;
+import com.softserve.academy.spaced.repetition.repository.DeckRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class TestController {
+    @Autowired
+    DeckRepository deckRepository;
 
     @RequestMapping("/test")
-    public String test(){
+    public String test() {
         return "test";
     }
-
-
 }
--- src/main/java/com/softserve/academy/spaced/repetition/repository/DeckRepository.java
+++ src/main/java/com/softserve/academy/spaced/repetition/repository/DeckRepository.java
@@ -2,13 +2,15 @@ package com.softserve.academy.spaced.repetition.repository;
 
 import com.softserve.academy.spaced.repetition.domain.Deck;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
-public interface DeckRepository extends JpaRepository<Deck, Long> {
+public interface DeckRepository extends JpaRepository <Deck, Long> {
 
-    List<Deck> getAllDecksByCategoryId(Long id);
+    List <Deck> getAllDecksByCategoryId(Long id);
 
-    List<Deck> findTop4ByOrderById();
+    List <Deck> findTop4ByOrderById();
 }
