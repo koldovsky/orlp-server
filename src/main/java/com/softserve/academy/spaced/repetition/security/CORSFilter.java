d0eefb03a6c0f32565df4276a55ba56fc6920069
Jarkin <jarkin.123789@gmail.com>
2017-07-21 17:42:22 +0300

    Add ReCaptcha and JwtSocialService

--- src/main/java/com/softserve/academy/spaced/repetition/security/CORSFilter.java
+++ src/main/java/com/softserve/academy/spaced/repetition/security/CORSFilter.java
@@ -1,5 +1,6 @@
 package com.softserve.academy.spaced.repetition.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
@@ -9,6 +10,9 @@ import java.io.IOException;
 @Component
 public class CORSFilter implements Filter {
 
+    @Value("${spring.origin.url}")
+    private String url;
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
     }
@@ -16,7 +20,7 @@ public class CORSFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Origin", url);
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
         response.setHeader("Access-Control-Allow-Credentials", "true");
