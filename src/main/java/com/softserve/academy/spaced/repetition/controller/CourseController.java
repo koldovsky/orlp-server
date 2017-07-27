29d6cb616ba60aa844af827979550dc82d05f8fb
zadorovskyi <zadorovskyi@gmail.com>
2017-07-27 11:44:14 +0300

    Top courses controller (#9)
    * changed path of authorization
    
    * refactored application.yml
    
    * Merge branch 'master' of https://github.com/koldovsky/orlp-server
    
    # Conflicts:
    #	src/main/java/com/softserve/academy/spaced/repetition/security/CORSFilter.java
    #	src/main/resources/application.yml
    
    * refactored code
    
    * changed expiration time -> expiration time moved to properties.yml

--- src/main/java/com/softserve/academy/spaced/repetition/controller/CourseController.java
+++ src/main/java/com/softserve/academy/spaced/repetition/controller/CourseController.java
@@ -4,13 +4,18 @@ import com.softserve.academy.spaced.repetition.DTO.DTOBuilder;
 import com.softserve.academy.spaced.repetition.DTO.impl.CourseLinkDTO;
 import com.softserve.academy.spaced.repetition.DTO.impl.CoursePublicDTO;
 import com.softserve.academy.spaced.repetition.DTO.impl.CourseTopDTO;
+import com.softserve.academy.spaced.repetition.DTO.impl.DeckPublicDTO;
 import com.softserve.academy.spaced.repetition.domain.Course;
+import com.softserve.academy.spaced.repetition.domain.Deck;
+import com.softserve.academy.spaced.repetition.service.AccessToUrlService;
 import com.softserve.academy.spaced.repetition.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -48,9 +53,11 @@ public class CourseController {
     @GetMapping("/api/course/top")
     public ResponseEntity<List<CourseTopDTO>> get4Course() {
         List<Course> courseList = courseService.get4Course();
-        Link collectionLink = linkTo(methodOn(CourseController.class).get4Course()).withSelfRel();
-        List<CourseTopDTO> courses = DTOBuilder.buildDtoListForCollection(courseList,
-                CourseTopDTO.class, collectionLink);
+        List<CourseTopDTO> courses = new ArrayList <>();
+        for (Course course : courseList) {
+            Link selfLink = linkTo(methodOn(CourseController.class).getCourseById(course.getCategory().getId(), course.getId())).withSelfRel();
+            courses.add(DTOBuilder.buildDtoForEntity(course, CourseTopDTO.class, selfLink));
+        }
         return new ResponseEntity<>(courses, HttpStatus.OK);
     }
 
