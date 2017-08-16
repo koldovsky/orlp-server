304462b0fc67d20922b677ed153e0db78dfdcf07
zadorovskyi <zadorovskyi@gmail.com>
2017-08-16 19:32:24 +0300

    Exceptions logging (#35)
    * changed path of authorization
    
    * refactored application.yml
    
    * Merge branch 'master' of https://github.com/koldovsky/orlp-server
    
    # Conflicts:
    #	src/main/java/com/softserve/academy/spaced/repetition/security/CORSFilter.java
    #	src/main/resources/application.yml
    
    * changed expiration time -> expiration time moved to properties.yml
    
    * added CourseLinkByUserDTO
    
    * added id field to DTO Course, added method to course controller
    
    * changed method of The CourseService updatingListOfUserCourses();
    
    * changed method of The CourseService updatingListOfUserCourses();
    
    * added changes userController, RegistrationController, CourseController;
    
    * Changed CourseServise
    
    * Changed CourseServise
    
    * added log4j, added exceptionLogger for public controllers and services
    
    * Added logging of the exceptions ORLP-117
    
    * deleted dublicated logs
    
    * moved log.txt to logs folder

---
+++ src/main/java/com/softserve/academy/spaced/repetition/logger/Logger.java
@@ -0,0 +1,5 @@
+package com.softserve.academy.spaced.repetition.logger;
+
+public interface Logger {
+    void log(String message);
+}
--- src/main/java/com/softserve/academy/spaced/repetition/service/RegistrationService.java
+++ src/main/java/com/softserve/academy/spaced/repetition/service/RegistrationService.java
@@ -3,6 +3,7 @@ package com.softserve.academy.spaced.repetition.service;
 import com.softserve.academy.spaced.repetition.domain.*;
 import com.softserve.academy.spaced.repetition.exceptions.BlankFieldException;
 import com.softserve.academy.spaced.repetition.exceptions.EmailUniquesException;
+import com.softserve.academy.spaced.repetition.logger.Logger;
 import com.softserve.academy.spaced.repetition.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,7 +15,6 @@ import org.springframework.stereotype.Service;
 import java.util.Calendar;
 import java.util.HashSet;
 import java.util.Set;
-
 
 @Service
 public class RegistrationService {
