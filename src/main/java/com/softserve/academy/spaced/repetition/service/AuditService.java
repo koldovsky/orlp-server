e61f19a5f891ca03842b94677812e325154188bc
LeOrK2312 <leorkestra@gmail.com>
2017-08-24 10:41:35 +0300

    improved code

--- src/main/java/com/softserve/academy/spaced/repetition/service/AuditService.java
+++ src/main/java/com/softserve/academy/spaced/repetition/service/AuditService.java
@@ -14,57 +14,57 @@ public class AuditService {
     @Autowired
     private AuditRepository auditRepository;
 
-    private List<Audit> getFullAuditList() {
+    public List<Audit> getFullAuditList() {
         List<Audit> auditList = auditRepository.findAll();
         return auditList;
     }
 
-    private List<Audit> getAuditListSortedByAccountEmailAsc() {
+    public List<Audit> getAuditListSortedByAccountEmailAsc() {
         List<Audit> auditList = auditRepository.findAllByOrderByAccountEmail();
         return auditList;
     }
 
-    private List<Audit> getAuditListSortedByAccountEmailDesc() {
+    public List<Audit> getAuditListSortedByAccountEmailDesc() {
         List<Audit> auditList = auditRepository.findAllByOrderByAccountEmailDesc();
         return auditList;
     }
 
-    private List<Audit> getAuditListSortedByActionAsc() {
+    public List<Audit> getAuditListSortedByActionAsc() {
         List<Audit> auditList = auditRepository.findAllByOrderByAction();
         return auditList;
     }
 
-    private List<Audit> getAuditListSortedByActionAscDesc() {
+    public List<Audit> getAuditListSortedByActionAscDesc() {
         List<Audit> auditList = auditRepository.findAllByOrderByActionDesc();
         return auditList;
     }
 
-    private List<Audit> getAuditListSortedByIpAddressAsc() {
+    public List<Audit> getAuditListSortedByIpAddressAsc() {
         List<Audit> auditList = auditRepository.findAllByOrderByIpAddress();
         return auditList;
     }
 
-    private List<Audit> getAuditListSortedByIpAddressDesc() {
+    public List<Audit> getAuditListSortedByIpAddressDesc() {
         List<Audit> auditList = auditRepository.findAllByOrderByIpAddressDesc();
         return auditList;
     }
 
-    private List<Audit> getAuditListSortedByTimeAsc() {
+    public List<Audit> getAuditListSortedByTimeAsc() {
         List<Audit> auditList = auditRepository.findAllByOrderByTime();
         return auditList;
     }
 
-    private List<Audit> getAuditListSortedByTimeDesc() {
+    public List<Audit> getAuditListSortedByTimeDesc() {
         List<Audit> auditList = auditRepository.findAllByOrderByTimeDesc();
         return auditList;
     }
 
-    private List<Audit> getAuditListSortedByRoleAsc() {
+    public List<Audit> getAuditListSortedByRoleAsc() {
         List<Audit> auditList = auditRepository.findAllByOrderByRole();
         return auditList;
     }
 
-    private List<Audit> getAuditListSortedByRoleDesc() {
+    public List<Audit> getAuditListSortedByRoleDesc() {
         List<Audit> auditList = auditRepository.findAllByOrderByRoleDesc();
         return auditList;
     }
