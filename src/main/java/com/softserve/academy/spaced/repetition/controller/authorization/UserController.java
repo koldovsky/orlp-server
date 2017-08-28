b051730f9ac9bd471b556bb2eccd3b50181c7d98
Vlad <vkiro@yandex.ua>
2017-08-28 12:00:57 +0300

    Create course: created method to get user by id.

--- src/main/java/com/softserve/academy/spaced/repetition/controller/authorization/UserController.java
+++ src/main/java/com/softserve/academy/spaced/repetition/controller/authorization/UserController.java
@@ -36,6 +36,14 @@ public class UserController {
         return new ResponseEntity <>(userDTO, HttpStatus.OK);
     }
 
+    @GetMapping("api/private/user/{user_id}")
+    public ResponseEntity <UserDTO> getUserById(@PathVariable Long user_id) {
+        User user = userService.getUserById(user_id);
+        Link link = linkTo(methodOn(UserController.class).getUserById(user_id)).withSelfRel();
+        UserDTO userDTO = DTOBuilder.buildDtoForEntity(user, UserDTO.class, link);
+        return new ResponseEntity <>(userDTO, HttpStatus.OK);
+    }
+
     @GetMapping("api/private/user")
     public ResponseEntity <UserLinksDTO> getAuthorizedUserWithLinks() {
         User user = userService.getAuthorizedUser();
