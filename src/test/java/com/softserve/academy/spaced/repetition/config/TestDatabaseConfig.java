34661c750fb3680bbc026f4a167061749441e7aa
antonbakalets <antonbakalets@yahoo.fr>
2017-10-17 14:23:25 +0300

    Service unit test example on H2 database. (#89)
    Service unit test example on H2 database.

---
+++ src/test/java/com/softserve/academy/spaced/repetition/config/TestDatabaseConfig.java
@@ -0,0 +1,47 @@
+package com.softserve.academy.spaced.repetition.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class TestDatabaseConfig {
+
+    @Primary
+    @Bean
+    public EmbeddedDatabase testDataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .build();
+    }
+
+    @Primary
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("com.softserve.academy.spaced.repetition.domain");
+
+        // drop-and-create database
+        Properties jpaProperties = new Properties();
+        jpaProperties.put("javax.persistence.schema-generation.database.action", "drop-and-create");
+        em.setJpaProperties(jpaProperties);
+
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setDatabasePlatform("org.hibernate.dialect.H2Dialect");
+        vendorAdapter.setShowSql(true);
+        em.setJpaVendorAdapter(vendorAdapter);
+
+        em.afterPropertiesSet();
+        return em;
+    }
+}
