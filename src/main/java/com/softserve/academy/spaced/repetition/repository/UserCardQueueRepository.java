ebd97ea47106b481ffad5d8e508fc7ec85a0d9ae
b0hdan <bohdan.dub@gmail.com>
2017-10-21 17:30:30 +0300

    Add icon with number of cards that need repeating to decks.

--- src/main/java/com/softserve/academy/spaced/repetition/repository/UserCardQueueRepository.java
+++ src/main/java/com/softserve/academy/spaced/repetition/repository/UserCardQueueRepository.java
@@ -4,7 +4,11 @@ import com.softserve.academy.spaced.repetition.domain.UserCardQueue;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+
 @Repository
 public interface UserCardQueueRepository extends JpaRepository<UserCardQueue, Long> {
     UserCardQueue findUserCardQueueByAccountEmailAndCardId(String accountEmail, Long cardId);
+
+    long countAllByAccountEmailEqualsAndDeckIdEqualsAndDateToRepeatBefore(String email, Long deckId, Date now);
 }
