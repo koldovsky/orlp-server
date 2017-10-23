d4bfe32acaa192375a03f83443f2979006e6a0b0
b0hdan <bohdan.dub@gmail.com>
2017-10-24 02:11:52 +0300

    Ability to switch learning regime has been added.

---
+++ src/main/java/com/softserve/academy/spaced/repetition/controller/AccountController.java
@@ -0,0 +1,42 @@
+package com.softserve.academy.spaced.repetition.controller;
+
+import com.softserve.academy.spaced.repetition.domain.LearningRegime;
+import com.softserve.academy.spaced.repetition.exceptions.NotAuthorisedUserException;
+import com.softserve.academy.spaced.repetition.repository.AccountRepository;
+import com.softserve.academy.spaced.repetition.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+    private final AccountRepository accountRepository;
+    private final AccountService accountService;
+
+    @Autowired
+    public AccountController(AccountRepository accountRepository, AccountService accountService) {
+        this.accountRepository = accountRepository;
+        this.accountService = accountService;
+    }
+
+    @GetMapping("/api/private/account/learning-regime")
+    public ResponseEntity<LearningRegime> getLearningRegime() throws NotAuthorisedUserException {
+        LearningRegime learningRegime = accountService.getLearningRegime();
+        return new ResponseEntity<>(learningRegime, HttpStatus.OK);
+    }
+
+    @PutMapping("/api/private/account/learning-regime")
+    public ResponseEntity updateLearningRegime(@RequestBody String learningRegime) throws NotAuthorisedUserException {
+        for (LearningRegime regime : LearningRegime.values()) {
+            if (regime.regime.equals(learningRegime)) {
+                accountService.updateLearningRegime(regime);
+                return new ResponseEntity(HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity(HttpStatus.BAD_REQUEST);
+    }
+}
--- src/main/java/com/softserve/academy/spaced/repetition/service/AccountService.java
+++ src/main/java/com/softserve/academy/spaced/repetition/service/AccountService.java
@@ -1,17 +1,34 @@
 package com.softserve.academy.spaced.repetition.service;
 
 import com.softserve.academy.spaced.repetition.domain.Account;
+import com.softserve.academy.spaced.repetition.domain.LearningRegime;
+import com.softserve.academy.spaced.repetition.exceptions.NotAuthorisedUserException;
 import com.softserve.academy.spaced.repetition.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountService {
-    @Autowired
-    AccountRepository accountRepository;
+    private final AccountRepository accountRepository;
+    private final UserService userService;
 
+    @Autowired
+    public AccountService(AccountRepository accountRepository, UserService userService) {
+        this.accountRepository = accountRepository;
+        this.userService = userService;
+    }
 
     public void updateAccount(Account account) {
+        accountRepository.save(account);
+    }
+
+    public LearningRegime getLearningRegime() throws NotAuthorisedUserException {
+        return userService.getAuthorizedUser().getAccount().getLearningRegime();
+    }
+
+    public void updateLearningRegime(LearningRegime learningRegime) throws NotAuthorisedUserException {
+        Account account = accountRepository.findOne(userService.getAuthorizedUser().getAccount().getId());
+        account.setLearningRegime(learningRegime);
         accountRepository.save(account);
     }
 }
