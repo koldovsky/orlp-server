bde2acce884063a79ebb6fdbe5cb9b41c7e7c0eb
IhorF <Ihor_F@i.ua>
2017-10-25 21:45:36 +0300

    ORLP-222.Added curly brackets

--- src/main/java/com/softserve/academy/spaced/repetition/domain/Person.java
+++ src/main/java/com/softserve/academy/spaced/repetition/domain/Person.java
@@ -89,8 +89,12 @@ public class Person {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Person person = (Person) o;
         return Objects.equals(this.id, person.id);
     }
