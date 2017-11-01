b4d541fcd9de9a6a002410f7c6e1767022b7a2a9
Volodymyr Tkachyk <vova1234.tkachik@gmail.com>
2017-11-01 12:57:01 +0200

    ORLP-230. Redirect from log.txt to orlp.log. (#125)
    * ORLP-230. Redirect from log.txt to orlp.log.
    
    * Deploy
    
    * ORLP-230. Redirect from log.txt to orlp.log.
    
    * ORLP-230. Redirect from log.txt to orlp.log.
    
    * ORLP-230. Redirect from log.txt to orlp.log. I hope last commit.
    
    * ORLP-230. Redirect from log.txt to orlp.log. I hope last commit.

--- src/main/java/com/softserve/academy/spaced/repetition/logger/LogExceptionAspect.java
+++ src/main/java/com/softserve/academy/spaced/repetition/logger/LogExceptionAspect.java
@@ -3,25 +3,24 @@ package com.softserve.academy.spaced.repetition.logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
 
 @Aspect
 @Component
-public class LogException {
-    @Autowired
-    Logger logger;
+public class LogExceptionAspect {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogExceptionAspect.class);
 
     @AfterThrowing(value = "execution(* com.softserve.academy.spaced.repetition.controller.*.*(..))", throwing = "ex")
     public void loggingExceptionsFromControllers(JoinPoint joinPoint, Exception ex) {
-        System.out.println(logger);
-        logger.log("Exception in controller = " + joinPoint.getThis() + ", method = " + joinPoint.getSignature());
+        LOGGER.error("Exception in controller = {} method = {}", joinPoint.getThis(), joinPoint.getSignature(), ex);
     }
 
     @AfterThrowing(value = "execution(* com.softserve.academy.spaced.repetition.service.*.*(..))", throwing = "ex")
     public void loggingExceptionsFromServices(JoinPoint joinPoint, Exception ex) {
-        System.out.println(logger);
-        logger.log("Exception in service = " + joinPoint.getThis() + ", method = " + joinPoint.getSignature());
+        LOGGER.error("Exception in service = {} method = {}", joinPoint.getThis(), joinPoint.getSignature(), ex);
     }
 
 }
