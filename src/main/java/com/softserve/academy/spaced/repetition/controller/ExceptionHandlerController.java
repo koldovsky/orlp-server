b9b4192caa1e9cff7e8700d0a7f714e0334cd5f6
Tamara20 <tamara.shyika@gmail.com>
2017-11-06 20:09:48 +0200

    ORLP-229 fix ExceptionHandlerController

--- src/main/java/com/softserve/academy/spaced/repetition/controller/ExceptionHandlerController.java
+++ src/main/java/com/softserve/academy/spaced/repetition/controller/ExceptionHandlerController.java
@@ -1,21 +1,17 @@
 package com.softserve.academy.spaced.repetition.controller;
 
-        import com.softserve.academy.spaced.repetition.DTO.impl.MessageDTO;
-        import com.softserve.academy.spaced.repetition.domain.AccountStatus;
-        import com.softserve.academy.spaced.repetition.exceptions.*;
-        import org.springframework.http.HttpStatus;
-        import org.springframework.http.ResponseEntity;
-        import org.springframework.mail.MailException;
-        import org.springframework.web.bind.annotation.ControllerAdvice;
-        import org.springframework.web.bind.annotation.ExceptionHandler;
-        import org.springframework.web.bind.annotation.ResponseBody;
-        import org.springframework.web.bind.annotation.ResponseStatus;
-        import org.springframework.web.multipart.MultipartException;
-        import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-        import javax.servlet.http.HttpServletRequest;
-        import java.net.UnknownHostException;
-        import java.util.*;
+import com.softserve.academy.spaced.repetition.DTO.impl.MessageDTO;
+import com.softserve.academy.spaced.repetition.domain.AccountStatus;
+import com.softserve.academy.spaced.repetition.exceptions.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MultipartException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import java.net.UnknownHostException;
+import java.util.*;
 
 @ControllerAdvice
 public class ExceptionHandlerController extends ResponseEntityExceptionHandler {
@@ -33,16 +29,9 @@ public class ExceptionHandlerController extends ResponseEntityExceptionHandler {
         return new ResponseEntity <>(new MessageDTO("File upload error: file is too large."), HttpStatus.FORBIDDEN);
     }
 
-    @ExceptionHandler(NullPointerException.class)
-    ResponseEntity <MessageDTO> handleIllegalValueOfRequestParameterException() {
-        return new ResponseEntity <>(new MessageDTO("The value of request parameter is not valid!"), HttpStatus.CONFLICT);
-    }
-
     @ExceptionHandler(RatingsBadValueException.class)
-    @ResponseBody
-    @ResponseStatus(value = HttpStatus.CONFLICT)
-    String handleRatingsBadValueException(HttpServletRequest request, Throwable ex) {
-        return "Rating can't be less than 1 and more than 5";
+    ResponseEntity <MessageDTO> handleRatingsBadValueException() {
+        return new ResponseEntity <>(new MessageDTO("Rating can't be less than 1 and more than 5"), HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(ImageRepositorySizeQuotaExceededException.class)
