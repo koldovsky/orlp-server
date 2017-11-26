ee2f80600bf9ff6d3ec36296905a63ffafbf725e
IgorF <farynaihor@gmail.com>
2017-11-26 19:55:57 +0200

    Added possibility for user to reset his password (#148)
    * Added possibility for user to reset his password
    
    * Changed mail template
    
    * Changed token creation, token verification and type of response
    
    * Changed template file extension to .html

--- src/main/java/com/softserve/academy/spaced/repetition/controller/AccountController.java
+++ src/main/java/com/softserve/academy/spaced/repetition/controller/AccountController.java
@@ -3,9 +3,13 @@ package com.softserve.academy.spaced.repetition.controller;
 import com.softserve.academy.spaced.repetition.domain.LearningRegime;
 import com.softserve.academy.spaced.repetition.domain.RememberingLevel;
 import com.softserve.academy.spaced.repetition.dto.DTOBuilder;
+import com.softserve.academy.spaced.repetition.dto.impl.NewAccountPasswordDTO;
 import com.softserve.academy.spaced.repetition.dto.impl.RememberingLevelDTO;
 import com.softserve.academy.spaced.repetition.exceptions.NotAuthorisedUserException;
 import com.softserve.academy.spaced.repetition.service.AccountService;
+import com.softserve.academy.spaced.repetition.service.AccountVerificationByEmailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +19,17 @@ import java.util.List;
 
 @RestController
 public class AccountController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AccountController.class);
     private final AccountService accountService;
 
     @Autowired
     public AccountController(AccountService accountService) {
         this.accountService = accountService;
     }
+
+    @Autowired
+    private AccountVerificationByEmailService verificationService;
 
     @GetMapping("/api/private/account/learning-regime")
     public ResponseEntity<LearningRegime> getLearningRegime() throws NotAuthorisedUserException {
@@ -60,6 +69,27 @@ public class AccountController {
     public ResponseEntity updateRememberingLevel(@PathVariable Long levelId,
                                                  @RequestBody String numberOfPostponedDays) throws NotAuthorisedUserException {
         accountService.updateRememberingLevel(levelId, Integer.parseInt(numberOfPostponedDays));
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping(value = "api/reset/password")
+    public ResponseEntity<String> sendResetPasswordMail(@RequestBody String email) {
+        LOGGER.debug("Send reset password mail to email: {}", email);
+        String accountStatus = accountService.checkAccountStatusAndSendMail(email);
+        return ResponseEntity.ok(accountStatus);
+    }
+
+    @PutMapping(value = "api/verification/token")
+    public ResponseEntity<String> verificationToken(@RequestBody String token) {
+        LOGGER.debug("Token verification");
+        String emailFromToken = verificationService.getAccountEmail(token);
+        return ResponseEntity.ok(emailFromToken);
+    }
+
+    @PutMapping(value = "api/create/password")
+    public ResponseEntity createNewPasswordForUser(@RequestBody NewAccountPasswordDTO newAccountPasswordDTO) {
+        LOGGER.debug("Created new password for: {}", newAccountPasswordDTO.getEmail());
+        accountService.createNewAccountPassword(newAccountPasswordDTO.getEmail(), newAccountPasswordDTO.getPassword());
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/softserve/academy/spaced/repetition/repository/AccountRepository.java
+++ src/main/java/com/softserve/academy/spaced/repetition/repository/AccountRepository.java
@@ -8,11 +8,15 @@ import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface AccountRepository extends JpaRepository <Account, Long> {
+public interface AccountRepository extends JpaRepository<Account, Long> {
     public Account findByEmail(String email);
 
     public Account findById(long id);
 
     @Query("UPDATE Account acc set acc.status = :status WHERE id = :account_id")
     public void updateAccount(@Param("status") String status, @Param("account_id") long accountId);
+
+    @Query("SELECT CASE WHEN COUNT(a) > 0 THEN TRUE ELSE FALSE END FROM Account a WHERE a.email = :email")
+    Boolean isEmailExists(@Param("email") String email);
+
 }
