bea6056e55169ca7522b4f1d05e2cccd0d693578
Roman Somyk <rsomyk@gmail.com>
2018-01-03 13:14:39 +0200

    WIP: Add Javadocs to all interfaces (second part)

--- src/main/java/com/softserve/academy/spaced/repetition/service/DeckCommentService.java
+++ src/main/java/com/softserve/academy/spaced/repetition/service/DeckCommentService.java
@@ -3,18 +3,52 @@ package com.softserve.academy.spaced.repetition.service;
 import com.softserve.academy.spaced.repetition.domain.Comment;
 import com.softserve.academy.spaced.repetition.domain.DeckComment;
 import com.softserve.academy.spaced.repetition.utils.exceptions.NotAuthorisedUserException;
+
 import java.util.List;
 
+/**
+ * This interface works with the comments to decks.
+ */
 public interface DeckCommentService {
-
+    /**
+     * Add a comment to decks
+     *
+     * @return true if operation is successful
+     */
     DeckComment addCommentForDeck(Long deckId, String commentText, Long parentCommentId)
             throws NotAuthorisedUserException;
 
+    /**
+     * Returns a comment with the given identifier
+     *
+     * @param commentId must not be {@literal null}.
+     */
     DeckComment getCommentById(Long commentId);
 
+    /**
+     * Returns all comments to the deck with the given identifier
+     *
+     * @param deckId must not be {@literal null}.
+     * @return list of comments to the deck with the given identifier
+     */
     List<Comment> getAllCommentsForDeck(Long deckId);
 
+    /**
+     * Updates comment with the given identifier with the new text
+     *
+     * @param commentId   must not be {@literal null}
+     * @param commentText must not be {null}
+     * @return true if operation is successful
+     */
+
     DeckComment updateCommentById(Long commentId, String commentText);
+
+    /**
+     * Delete comments to the deck with the given identifier
+     *
+     * @param commentId must not be {@literal null}
+     * @return true if operation is successful
+     */
 
     void deleteCommentById(Long commentId);
 }
