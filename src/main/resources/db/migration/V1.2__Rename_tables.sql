053a58253f2e5c7010f7b0c9afb4db0e7b75d9a7
Serhii Venhrynovych <serhiivenhrynovych@gmail.com>
2018-01-27 16:50:23 +0200

    ORLP-432 migration script added

--- src/main/resources/db/migration/V1.2__Rename_tables.sql
+++ src/main/resources/db/migration/V1.2__Rename_tables.sql
@@ -1,12 +1,12 @@
 alter table account change authenticationtype authentication_type varchar(8) not null;
 alter table account change lastpasswordresetdate last_password_reset_date varchar(255) not null;
 alter table account change email email varchar(254) not null;
+alter table account change password password varchar(60) not null;
 alter table audit change account_email account_email varchar(254) not null;
 alter table card_rating change account_email account_email varchar(254) not null;
 alter table course_rating change account_email account_email varchar(254) not null;
 alter table deck change synthax syntax varchar(255);
 alter table deck_rating change account_email account_email varchar(254) not null;
-alter table image imagebase64 image_base64 LONGTEXT;
-alter table person imagebase64 image_base64 LONGTEXT;
-alter table person imagetype image_type varchar(6) not null;
-
+alter table image change imagebase64 image_base64 LONGTEXT;
+alter table person change imagebase64 image_base64 LONGTEXT;
+alter table person change imagetype image_type varchar(6) not null;
