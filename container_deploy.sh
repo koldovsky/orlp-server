44b9188879f668810f70c711b3429126af3f5ed6
ZenykK <zenoviy.kharkhalis@gmail.com>
2018-02-02 00:24:59 +0200

    Changed hostname in url of jdbc connector driver

--- container_deploy.sh
+++ container_deploy.sh
@@ -12,11 +12,13 @@ if [ ! "$(docker ps -aq -f name=$mysql_container)" ]; then
     docker run --name $mysql_container --net=orlp --ip=192.168.0.50 -h $mysql_container --expose=3306 -e MYSQL_ROOT_PASSWORD=123456 -d mysql:latest
 fi
 
+sleep 3;
+
 if [ ! "$(docker ps -aq -f name=$container_name)" ]; then
     echo "Container $container_name is not running. Starting $container_name ...";
-    docker run --name $container_name --net=orlp --add-host orlp-mysql:192.168.0.50 -p 8080:8080 --expose=1443 -e 'VIRTUAL_HOST=infolve.com:8080' -e 'VIRTUAL_PROTO=https' -v /home/wercker/orlp-server:/usr/src/myapp -w /usr/src/myapp java:8-jre java -jar Spaced.Repetition.jar;
+    docker run --name $container_name -d --net=orlp --add-host orlp-mysql:192.168.0.50 -p 8080:8080 --expose=1443 -e 'VIRTUAL_HOST=infolve.com:8080' -e 'VIRTUAL_PROTO=https' -v /home/wercker/orlp-server:/usr/src/myapp -w /usr/src/myapp java:8-jre java -jar Spaced.Repetition.jar;
     else
     echo "$container_name already running. Restarting $container_name ...";
     docker container stop $container_name && docker container rm -f $container_name;
-    docker run --name $container_name --net=orlp --add-host orlp-mysql:192.168.0.50 -p 8080:8080 --expose=1443 -e 'VIRTUAL_HOST=infolve.com:8080' -e 'VIRTUAL_PROTO=https' -v /home/wercker/orlp-server:/usr/src/myapp -w /usr/src/myapp java:8-jre java -jar Spaced.Repetition.jar;
+    docker run --name $container_name -d --net=orlp --add-host orlp-mysql:192.168.0.50 -p 8080:8080 --expose=1443 -e 'VIRTUAL_HOST=infolve.com:8080' -e 'VIRTUAL_PROTO=https' -v /home/wercker/orlp-server:/usr/src/myapp -w /usr/src/myapp java:8-jre java -jar Spaced.Repetition.jar;
 fi
