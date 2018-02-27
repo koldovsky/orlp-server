b7627c5c087057c1b1ec10fa56b5190f035f22cf
Roman Somyk <rsomyk@gmail.com>
2018-02-27 18:40:36 +0200

    ORLP-429 - Security registration controller fix

--- src/main/java/com/softserve/academy/spaced/repetition/controller/RegistrationController.java
+++ src/main/java/com/softserve/academy/spaced/repetition/controller/RegistrationController.java
@@ -18,6 +18,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -37,6 +38,7 @@ public class RegistrationController {
 
     @Auditable(action = AuditingAction.SIGN_UP)
     @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    @PreAuthorize("!isAuthenticated()")
     public ResponseEntity<Person> addUser(@Validated(Request.class) @RequestBody User userFromClient) {
         User user = registrationService.registerNewUser(userFromClient);
         registrationService.sendConfirmationEmailMessage(user);
