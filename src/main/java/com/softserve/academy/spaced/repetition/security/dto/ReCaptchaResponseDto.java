dbd2f11ee4d9d96bf5c23c1996d6acc3c940365a
Oleh <0leh.palyx@gmail.com>
2018-09-27 11:50:03 +0300

    Deleted line before import

--- src/main/java/com/softserve/academy/spaced/repetition/security/dto/ReCaptchaResponseDto.java
+++ src/main/java/com/softserve/academy/spaced/repetition/security/dto/ReCaptchaResponseDto.java
@@ -3,7 +3,6 @@ package com.softserve.academy.spaced.repetition.security.DTO;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
