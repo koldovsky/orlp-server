3694df821314c2129a191725016a852e7734d19a
Anton Bakalets <antonbakalets@yahoo.fr>
2018-09-28 11:23:52 +0300

    ORLP. Fixed missing import.

--- src/main/java/com/softserve/academy/spaced/repetition/controller/UserController.java
+++ src/main/java/com/softserve/academy/spaced/repetition/controller/UserController.java
@@ -6,13 +6,16 @@ import com.softserve.academy.spaced.repetition.controller.dto.impl.UserDTO;
 import com.softserve.academy.spaced.repetition.controller.dto.impl.UserLinksDTO;
 import com.softserve.academy.spaced.repetition.domain.Course;
 import com.softserve.academy.spaced.repetition.domain.User;
-import com.softserve.academy.spaced.repetition.utils.exceptions.NotAuthorisedUserException;
 import com.softserve.academy.spaced.repetition.service.UserService;
+import com.softserve.academy.spaced.repetition.utils.exceptions.NotAuthorisedUserException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
