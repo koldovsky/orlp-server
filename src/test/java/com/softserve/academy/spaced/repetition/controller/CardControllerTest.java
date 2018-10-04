ab054e17d585b960ca07b2380de59ba89af92641
Oleh Paliukh <0leh.palyx@gmail.com>
2018-10-04 15:32:07 +0300

    Closes #249 - Create Unit Tests (Part 1)

--- src/test/java/com/softserve/academy/spaced/repetition/controller/CardControllerTest.java
+++ src/test/java/com/softserve/academy/spaced/repetition/controller/CardControllerTest.java
@@ -1,6 +1,5 @@
 package com.softserve.academy.spaced.repetition.controller;
 
-
 import com.softserve.academy.spaced.repetition.controller.handler.ExceptionHandlerController;
 import com.softserve.academy.spaced.repetition.domain.*;
 import com.softserve.academy.spaced.repetition.service.CardService;
@@ -20,8 +19,7 @@ import java.util.List;
 
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -38,6 +36,14 @@ public class CardControllerTest {
     @Mock
     private UserService mockedUserService;
 
+    private String QUESTION = "How many access modifiers do you know in Java?";
+    private String ANSWER = "There are 4 access modifiers in Java: public, protected, default and private";
+    private String CARD_IMAGE = "";
+    private String TITLE= "Card 11";
+    private String CARD_TO_ADD = String.format("{ \"answer\": \"%s\"," +"\"image\": " + " [ "+
+            "\"%s\" "+"],"+"\"question\": \"%s\", " + "\"title\": \"%s\" }",
+            ANSWER, CARD_IMAGE, QUESTION, TITLE);
+
     private static final long RATING = 0L;
     private static final long DECK_ID = 1L;
 
@@ -46,6 +52,34 @@ public class CardControllerTest {
         mockMvc = MockMvcBuilders.standaloneSetup(cardController)
                 .setControllerAdvice(new ExceptionHandlerController())
                 .build();
+    }
+
+    @Test
+    public void addCard() throws Exception {
+        mockMvc.perform(post("/api/decks/{deckId}/cards", 1L)
+                .content(CARD_TO_ADD)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void areThereNotPostponedCardsAvailable()throws Exception {
+        mockMvc.perform(get("/api/decks/{deckId}/not-postponed" , 1L))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void updateCard()throws Exception{
+        mockMvc.perform(put("/api/decks/{deckId}/cards/{cardId}" , 1L, 190L)
+                .content(CARD_TO_ADD)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void deleteCard()throws Exception{
+        mockMvc.perform(delete("/api/decks/{deckId}/cards/{cardId}",1L,1L))
+                .andExpect(status().isOk());
     }
 
     @Test
@@ -67,6 +101,141 @@ public class CardControllerTest {
                         "          \"href\":\"http://localhost/api/decks/1/cards/1\"" +
                         "      }]" +
                         "}"));
+    }
+
+    @Test
+    public void getCardsByDeck() throws Exception {
+        Long deckId = 1L;
+        when(cardService.findAllByDeckId(deckId)).thenReturn(createLearningCards());
+        mockMvc.perform(get("/api/decks/{deckId}/cards", 1L)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[" +
+                        "{" +
+                        "  \"question\": \"How many access modifiers do you know in Java?\"," +
+                        "   \"rating\": 0," +
+                        "  \"answer\": \"There are 4 access modifiers in Java: public, protected, default and private\"," +
+                        "  \"title\": \"Card 1\"," +
+                        "  \"cardId\": 1," +
+                        "   \"links\": [{" +
+                        "        \"rel\":\"self\"," +
+                        "          \"href\":\"http://localhost/api/decks/1/cards/1\"" +
+                        "      }]" +
+                        "}," +
+                        "{" +
+                        " \"question\": \"What are the supported platforms by Java Programming Language?\"," +
+                        "  \"rating\": 0," +
+                        " \"answer\": \"Java runs on a variety of platforms, such as Windows, Mac OS, and the various" +
+                        " versions of UNIX/Linux like HP-Unix, Sun Solaris, Redhat Linux, Ubuntu, CentOS, etc.\"," +
+                        "  \"title\": \"Card 2\"," +
+                        " \"cardId\": 2," +
+                        "   \"links\": [{" +
+                        "        \"rel\":\"self\"," +
+                        "          \"href\":\"http://localhost/api/decks/1/cards/2\"" +
+                        "      }]" +
+                        "}," +
+                        "{" +
+                        "  \"question\": \"List any five features of Java?\"," +
+                        "   \"rating\": 0," +
+                        "  \"answer\": \"Some features include Object Oriented, Platform Independent, Robust, " +
+                        "Interpreted, Multi-threaded\"," +
+                        "  \"title\": \"Card 3\"," +
+                        "  \"cardId\": 3," +
+                        "   \"links\": [{" +
+                        "        \"rel\":\"self\"," +
+                        "          \"href\":\"http://localhost/api/decks/1/cards/3\"" +
+                        "      }]" +
+                        "}," +
+                        "{" +
+                        "  \"question\": \"Why is Java Architectural Neutral?\"," +
+                        "   \"rating\": 0," +
+                        "  \"answer\": \"It’s compiler generates an architecture-neutral object file format, which " +
+                        "makes the compiled code to be executable on many processors, with the presence of Java runtime" +
+                        " system.\"," +
+                        "  \"title\": \"Card 4\"," +
+                        "  \"cardId\": 4," +
+                        "   \"links\": [{" +
+                        "        \"rel\":\"self\"," +
+                        "          \"href\":\"http://localhost/api/decks/1/cards/4\"" +
+                        "      }]" +
+                        "}," +
+                        "{" +
+                        "  \"question\": \"How Java enabled High Performance?\"," +
+                        "   \"rating\": 0," +
+                        "  \"answer\": \"Java uses Just-In-Time compiler to enable high performance. Just-In-Time " +
+                        "compiler is a program that turns Java bytecode, which is a program that contains instructions" +
+                        " that must be interpreted into instructions that can be sent directly to the processor.\"," +
+                        "  \"title\": \"Card 5\"," +
+                        "  \"cardId\": 5," +
+                        "   \"links\": [{" +
+                        "        \"rel\":\"self\"," +
+                        "          \"href\":\"http://localhost/api/decks/1/cards/5\"" +
+                        "      }]" +
+                        "}," +
+                        "{" +
+                        "  \"question\": \"Why Java is considered dynamic?\"," +
+                        "   \"rating\": 0," +
+                        "  \"answer\": \"It is designed to adapt to an evolving environment. Java programs can carry" +
+                        " extensive amount of run-time information that can be used to verify and resolve accesses" +
+                        " to objects on run-time.\"," +
+                        "  \"title\": \"Card 6\"," +
+                        "  \"cardId\": 6," +
+                        "   \"links\": [{" +
+                        "        \"rel\":\"self\"," +
+                        "          \"href\":\"http://localhost/api/decks/1/cards/6\"" +
+                        "      }]" +
+                        "}," +
+                        "{" +
+                        "  \"question\": \"What is Java Virtual Machine and how it is considered in context of Java’s" +
+                        " platform independent feature?\"," +
+                        "   \"rating\": 0," +
+                        "  \"answer\": \"When Java is compiled, it is not compiled into platform specific machine," +
+                        " rather into platform independent byte code. This byte code is distributed over the web and" +
+                        " interpreted by virtual Machine (JVM) on whichever platform it is being run.\"," +
+                        "  \"title\": \"Card 7\"," +
+                        "  \"cardId\": 7," +
+                        "   \"links\": [{" +
+                        "        \"rel\":\"self\"," +
+                        "          \"href\":\"http://localhost/api/decks/1/cards/7\"" +
+                        "      }]" +
+                        "}," +
+                        "{" +
+                        "  \"question\": \"List two Java IDE’s?\"," +
+                        "   \"rating\": 0," +
+                        "  \"answer\": \"Netbeans, Eclipse, etc.\"," +
+                        "  \"title\": \"Card 8\"," +
+                        "  \"cardId\": 8," +
+                        "   \"links\": [{" +
+                        "        \"rel\":\"self\"," +
+                        "          \"href\":\"http://localhost/api/decks/1/cards/8\"" +
+                        "      }]" +
+                        "}," +
+                        "{" +
+                        "  \"question\": \"List some Java keywords(unlike C, C++ keywords)?\"," +
+                        "   \"rating\": 0," +
+                        "  \"answer\": \"Some Java keywords are import, super, finally, etc.\"," +
+                        "  \"title\": \"Card 9\"," +
+                        "  \"cardId\": 9," +
+                        "   \"links\": [{" +
+                        "        \"rel\":\"self\"," +
+                        "          \"href\":\"http://localhost/api/decks/1/cards/9\"" +
+                        "      }]" +
+                        "}," +
+                        "{" +
+                        "  \"question\": \"What do you mean by Object?\"," +
+                        "   \"rating\": 0," +
+                        "  \"answer\": \"Object is a runtime entity and it’s state is stored in fields and behavior" +
+                        " is shown via methods. Methods operate on an objects internal state and serve as the" +
+                        " primary mechanism for object-to-object communication.\"," +
+                        "  \"title\": \"Card 10\"," +
+                        "  \"cardId\": 10," +
+                        "   \"links\": [{" +
+                        "        \"rel\":\"self\"," +
+                        "          \"href\":\"http://localhost/api/decks/1/cards/10\"" +
+                        "      }]" +
+                        "}" +
+                        "]"));
     }
 
     @Test
@@ -206,6 +375,70 @@ public class CardControllerTest {
                         "]"));
     }
 
+    @Test
+    public void getAdditionalLearningCards() throws Exception {
+        Long deckId = 2L;
+        User mockedUser1 = new User(new Account("","email1@email.com"), new Person("first1", "last1"), new Folder());
+        when(mockedUserService.getAuthorizedUser()).thenReturn(mockedUser1);
+        when(cardService.getAdditionalLearningCards(deckId)).thenReturn(createAdditionalLearningCards());
+        mockMvc.perform(get("/api/decks/{deckId}/learn/additional", 2L)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(
+                        "[" +
+                        "{" +
+                        "  \"question\": \"How many access modifiers do you know in Java?\"," +
+                        "   \"rating\": 0," +
+                        "  \"answer\": \"There are 4 access modifiers in Java: public, protected, default and private\"," +
+                        "  \"title\": \"Card 1\"," +
+                        "  \"cardId\": 1," +
+                        "   \"links\": [{" +
+                        "        \"rel\":\"self\"," +
+                        "          \"href\":\"http://localhost/api/decks/2/cards/1\"" +
+                        "      }]" +
+                        "}," +
+                        "{" +
+                        "  \"question\": \"List any five features of Java?\"," +
+                        "   \"rating\": 0," +
+                        "  \"answer\": \"Some features include Object Oriented, Platform Independent, Robust, " +
+                        "Interpreted, Multi-threaded\"," +
+                        "  \"title\": \"Card 3\"," +
+                        "  \"cardId\": 3," +
+                        "   \"links\": [{" +
+                        "        \"rel\":\"self\"," +
+                        "          \"href\":\"http://localhost/api/decks/2/cards/3\"" +
+                        "      }]" +
+                        "}," +
+                        "{" +
+                        "  \"question\": \"Why is Java Architectural Neutral?\"," +
+                        "   \"rating\": 0," +
+                        "  \"answer\": \"It’s compiler generates an architecture-neutral object file format, which " +
+                        "makes the compiled code to be executable on many processors, with the presence of Java runtime" +
+                        " system.\"," +
+                        "  \"title\": \"Card 4\"," +
+                        "  \"cardId\": 4," +
+                        "   \"links\": [{" +
+                        "        \"rel\":\"self\"," +
+                        "          \"href\":\"http://localhost/api/decks/2/cards/4\"" +
+                        "      }]" +
+                        "}," +
+                        "{" +
+                        "  \"question\": \"Why Java is considered dynamic?\"," +
+                        "   \"rating\": 0," +
+                        "  \"answer\": \"It is designed to adapt to an evolving environment. Java programs can carry" +
+                        " extensive amount of run-time information that can be used to verify and resolve accesses" +
+                        " to objects on run-time.\"," +
+                        "  \"title\": \"Card 6\"," +
+                        "  \"cardId\": 6," +
+                        "   \"links\": [{" +
+                        "        \"rel\":\"self\"," +
+                        "          \"href\":\"http://localhost/api/decks/2/cards/6\"" +
+                        "      }]" +
+                        "}" +
+                        "]"));
+    }
+
     private Card createCard(Long id, String title, String question, String answer, Deck deck) {
         Card card = new Card();
         card.setId(id);
@@ -228,6 +461,34 @@ public class CardControllerTest {
         card.setDeck(deck);
         card.setRating(RATING);
         return card;
+    }
+
+    private List<Card> createAdditionalLearningCards() {
+        List<Card> cards = new ArrayList<>();
+        Deck deck = new Deck();
+       deck.setId(DECK_ID);
+
+        Card card = createCard(1L, "Card 1", "How many access modifiers do you know in Java?",
+                "There are 4 access modifiers in Java: public, protected, default and private", deck);
+
+        Card card3 = createCard(3L, "Card 3", "List any five features of Java?", "Some " +
+                "features include Object Oriented, Platform Independent, Robust, Interpreted, Multi-threaded", deck);
+
+        Card card4 = createCard(4L, "Card 4", "Why is Java Architectural Neutral?",
+                "It’s compiler generates an architecture-neutral object file format, which makes the" +
+                        " compiled code to be executable on many processors, with the presence of Java runtime system.",
+                deck);
+
+        Card card6 = createCard(6L, "Card 6", "Why Java is considered dynamic?", "It is " +
+                "designed to adapt to an evolving environment. Java programs can carry extensive amount of run-time" +
+                " information that can be used to verify and resolve accesses to objects on run-time.", deck);
+
+        cards.add(card);
+        cards.add(card3);
+        cards.add(card4);
+        cards.add(card6);
+
+        return cards;
     }
 
     private List<Card> createLearningCards() {
