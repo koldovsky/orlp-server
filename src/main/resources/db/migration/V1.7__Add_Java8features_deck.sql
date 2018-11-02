8db860f4b1f92a4f5fe68408ba2cc3525143c9dd
Maksym Turko <uaillusion@gmail.com>
2018-11-02 21:10:58 +0200

    Added a template for a script, still needs work

---
+++ src/main/resources/db/migration/V1.7__Add_Java8features_deck.sql
@@ -0,0 +1,45 @@
+insert into deck (description, name, rating, category_id, user_id, syntax, created_by) values ('Questions regarding Features that were implemented in Java 8', 'Java 8 Features', 0, 1, 1, 'JAVA', 1);
+
+insert into card (answer, question, rating, title, deck_id, created_by) values ('String G7Countries = G7.stream().map(x -> x.toUpperCase()).collect(Collectors.joining(", ")); System.out.println(G7Countries);', 'Convert String to UpperCase and join them using coma List<String> G7 = Arrays.asList("USA", "Japan", "France", "Germany", "Italy", "U.K.","Canada");', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('The key difference between Anonymous class and Lambda expression is the usage of ‘this’ keyword. In the anonymous classes, ‘this’ keyword resolves to anonymous class itself, whereas for lambda expression ‘this’ keyword resolves to enclosing class where lambda expression is written. Another difference between lambda expression and anonymous class is in the way these two are compiled. Java compiler compiles lambda expressions and convert them into private method of the class. It uses invokedynamic instruction that was added in Java 7 to bind this method dynamically.', 'What is difference between Lambda Expression and Anonymous class?', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('public class RunnableLambda { public static void main(String[] args) { // Runnable using lambda new Thread(()->System.out.println("Runnable as Lambda expression")).start(); }}', 'How can we write runnable as lambda expression? public class RunnableIC { public static void main(String[] args) { // Runnable using anonymous class new Thread(new Runnable() { @Override public void run() { System.out.println("Runnable as anonymous class"); } }).start();  }}', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+insert into card (answer, question, rating, title, deck_id, created_by) values ('', '', 0, 'Card 1', 12, 1);
+
+
