c6d1368f6fe4ec6bffe01e103072b24155e88f5a
OleksandrMazur <al3x.mazur2@gmail.com>
2018-11-09 10:26:55 +0200

    Updated V1.9__Create_DeckPrice_table.sql migration script with default value for deck_price

--- src/main/resources/db/migration/V1.9__Create_DeckPrice_table.sql
+++ src/main/resources/db/migration/V1.9__Create_DeckPrice_table.sql
@@ -1,7 +1,7 @@
 create table deck_price (
     deck_price_id bigint not null auto_increment,
     deck_id bigint,
-    deck_price integer not null,
+    deck_price integer default null,
     primary key (deck_price_id)
 );
 
