a4d734110e34e1984de0b2c744c91d9806b71f71
volodymyrglynka <v.glynka@gmail.com>
2018-11-11 17:55:38 +0200

    Created PointsTransaction entity, DAO, table

---
+++ src/main/resources/db/migration/V1.11__Add_PointsTransaction_table_and_fill_it.sql
@@ -0,0 +1,18 @@
+create table points_transaction (
+  transaction_id bigint not null auto_increment,
+  reference_id bigint not null,
+  date_creation datetime not null,
+  user_from bigint not null,
+  user_to bigint not null,
+  points int not null ,
+  primary key (transaction_id)
+);
+
+  alter table points_transaction add constraint POINTS_TRANSACTION_TRANSACTION_FK foreign key (reference_id) references points_transaction (transaction_id);
+  alter table points_transaction add constraint POINTS_TRANSACTION_USER_FROM_FK foreign key (user_from) references user (user_id);
+  alter table points_transaction add constraint POINTS_TRANSACTION_USER_TO_FK foreign key (user_to) references user (user_id);
+
+  insert into points_transaction (reference_id, date_creation, user_from, user_to, points) values (1, '2018-11-11 17:25:46', 1, 3, 1452);
+  insert into points_transaction (reference_id, date_creation, user_from, user_to, points) values (2, '2018-10-19 12:58:15', 2, 1, 2215);
+  insert into points_transaction (reference_id, date_creation, user_from, user_to, points) values (3, '2018-09-13 15:27:54', 4, 2, 1897);
+  insert into points_transaction (reference_id, date_creation, user_from, user_to, points) values (4, '2018-10-22 20:25:09', 2, 3, 1254);
