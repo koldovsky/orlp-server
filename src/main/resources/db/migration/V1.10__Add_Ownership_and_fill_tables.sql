6b68c808103d23495f22dd12c6939a8cfb92b473
Oleh <0leh.palyx@gmail.com>
2018-11-11 14:11:48 +0200

    Changed migration file name and fill tables

--- src/main/resources/db/migration/V1.10__Add_Ownership_and_fill_tables.sql
+++ src/main/resources/db/migration/V1.10__Add_Ownership_and_fill_tables.sql
@@ -23,3 +23,13 @@ alter table deck_ownership add constraint Deck_Ownership_Deck_FK foreign key (de
 alter table course_ownership add constraint Course_Ownership_Transaction_FK foreign key (reference_id) references course_ownership (transaction_id);
 alter table course_ownership add constraint Course_Ownership_User_FK foreign key (user_id) references user (user_id);
 alter table course_ownership add constraint Course_Ownership_Deck_FK foreign key (course_id) references course (course_id);
+
+INSERT INTO deck_ownership(reference_id,date_creation,user_id,deck_id) values (1,'2018-11-11', 2, 1);
+INSERT INTO deck_ownership(reference_id,date_creation,user_id,deck_id) values (2,'2018-11-12', 3, 2);
+INSERT INTO deck_ownership(reference_id,date_creation,user_id,deck_id) values (3,'2018-11-13', 3, 3);
+INSERT INTO deck_ownership(reference_id,date_creation,user_id,deck_id) values (4,'2018-11-14', 2, 4);
+
+INSERT INTO course_ownership(reference_id,date_creation,user_id,course_id) values (1,'2018-11-11', 2, 1);
+INSERT INTO course_ownership(reference_id,date_creation,user_id,course_id) values (2,'2018-11-12', 3, 2);
+INSERT INTO course_ownership(reference_id,date_creation,user_id,course_id) values (3,'2018-11-13', 3, 3);
+INSERT INTO course_ownership(reference_id,date_creation,user_id,course_id) values (4,'2018-11-14', 2, 4);
