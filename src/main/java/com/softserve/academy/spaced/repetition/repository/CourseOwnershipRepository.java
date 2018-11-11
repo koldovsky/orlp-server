a5b413cc2d0e42ad4368d62c23e5bd561fe338f5
OlehPaliuh <32176964+OlehPaliuh@users.noreply.github.com>
2018-11-11 17:48:29 +0200

    Closes #308 Create ownership table, Entity, DAO

---
+++ src/main/java/com/softserve/academy/spaced/repetition/repository/CourseOwnershipRepository.java
@@ -0,0 +1,7 @@
+package com.softserve.academy.spaced.repetition.repository;
+
+import com.softserve.academy.spaced.repetition.domain.CourseOwnership;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseOwnershipRepository extends JpaRepository<CourseOwnership,Long> {
+}
