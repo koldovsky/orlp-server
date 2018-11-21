a03a8f11e94bc96f563be89b0ee87076780ee542
volodymyrglynka <v.glynka@gmail.com>
2018-11-21 19:41:01 +0200

    Added ResponseEntity

--- src/main/java/com/softserve/academy/spaced/repetition/controller/PointsTransactionController.java
+++ src/main/java/com/softserve/academy/spaced/repetition/controller/PointsTransactionController.java
@@ -3,6 +3,8 @@ package com.softserve.academy.spaced.repetition.controller;
 import com.softserve.academy.spaced.repetition.controller.dto.simpleDTO.userProfileDTO.SendPointsToFriendDTO;
 import com.softserve.academy.spaced.repetition.service.PointsTransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +16,8 @@ public class PointsTransactionController {
     PointsTransactionService pointsTransactionService;
 
     @PutMapping(value = "api/profile/wallet")
-    public SendPointsToFriendDTO sendPointsToFriend (@RequestBody SendPointsToFriendDTO sendPointsToFriendDTO) {
-        return pointsTransactionService.sendPointsToFriend(sendPointsToFriendDTO);
+    public ResponseEntity<SendPointsToFriendDTO> sendPointsToFriend (@RequestBody SendPointsToFriendDTO sendPointsToFriendDTO) {
+        SendPointsToFriendDTO pointsToFriendDTO  = pointsTransactionService.sendPointsToFriend(sendPointsToFriendDTO);
+        return new ResponseEntity<>(pointsToFriendDTO, HttpStatus.OK);
     }
 }
