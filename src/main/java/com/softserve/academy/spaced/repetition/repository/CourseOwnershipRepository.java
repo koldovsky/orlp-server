956a2bcaa4f111537a06abc392a25552b41564b6
Maks-Chornyi <380960310411@mail.ua>
2018-11-21 17:19:15 +0200

    updated code from develop

--- src/main/java/com/softserve/academy/spaced/repetition/repository/CourseOwnershipRepository.java
+++ src/main/java/com/softserve/academy/spaced/repetition/repository/CourseOwnershipRepository.java
@@ -4,4 +4,6 @@ import com.softserve.academy.spaced.repetition.domain.CourseOwnership;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CourseOwnershipRepository extends JpaRepository<CourseOwnership,Long> {
+
+    void deleteCourseOwnershipByCourseId(Long courseId);
 }
